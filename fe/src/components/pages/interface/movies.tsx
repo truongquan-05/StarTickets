4316426bba2675ee349bd9b4c816637e3778b75c
truongquan-05 <quanctph50803@gmail.com>
2025-05-29 23:38:10 +0700

    new

--- fe/src/components/pages/interface/movies.tsx
+++ fe/src/components/pages/interface/movies.tsx
@@ -1,35 +1,3 @@
-<<<<<<< .merge_file_xZSEPU
-export interface IMovies{
-  id : number
-ten_phim : string
-mo_ta : string
-thoi_luong : number
-trailer : string
-ngon_ngu : string
-quoc_gia : string
-anh_poster : string
-ngay_cong_chieu : string
-tinh_trang : string
-do_tuoi_gioi_han : number
-trang_thai : boolean
-the_loai_id : number
-}
-
-export interface MoviesForm{
-  ten_phim : string
-mo_ta : string
-thoi_luong : number
-trailer : string
-ngon_ngu : string
-quoc_gia : string
-anh_poster : string
-ngay_cong_chieu : string
-tinh_trang : string
-do_tuoi_gioi_han : number
-trang_thai : boolean
-the_loai_id : number
-}
-=======
 export interface IMovies {
   id: number;
   ten_phim: string;
@@ -60,4 +28,4 @@ export interface MoviesForm {
   trang_thai: boolean;
   the_loai_id: number;
 }
->>>>>>> .merge_file_v1pCEB
+
