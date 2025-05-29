8b83d43838edc40aa55c9d214624ffc7d5b6b6eb
Trần Tuấn Hưng <hungttph52525@gmail.com>
2025-05-29 15:33:27 +0700

    fix2

--- src/components/assets/js/indexAdmin.js
+++
@@ -1,4 +0,0 @@
-// document.getElementById("toggle-btn").addEventListener("click", function() {
-//   document.querySelector(".sider").classList.toggle("collapsed");
-//   document.querySelector(".content").classList.toggle("collapsed");
-// });
