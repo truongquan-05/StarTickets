82dfc53ee5d0e9734cb7cd405dc454ea03015d8e
truongquan-05 <quanctph50803@gmail.com>
2025-07-09 14:08:24 +0700

    update seed

--- BE/database/seeders/VaiTroSeeder.php
+++ BE/database/seeders/VaiTroSeeder.php
@@ -11,13 +11,22 @@ class VaiTroSeeder extends Seeder
     {
         DB::table('vai_tro')->insert([
             [
-                'id' => 2,
-                'ten_vai_tro' => 'User', 
-            ],
-            [
                 'id' => 1,
                 'ten_vai_tro' => 'Admin', // hoặc tên vai trò phù hợp
-            ]
+            ],
+            [
+                'id' => 2,
+                'ten_vai_tro' => 'User',
+            ],
+            [
+                'id' => 3,
+                'ten_vai_tro' => 'Nhân viên',
+            ],
+            [
+                'id' => 4,
+                'ten_vai_tro' => 'Nhân viên rạp',
+            ],
+
         ]);
     }
 }
