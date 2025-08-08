01fb33be5f68f514033d5fd698e85c2e47bd8e64
Vinh-tech05 <vinhpcph50853@gmail.com>
2025-08-08 16:51:00 +0700

    Fix dangGia

---
+++ BE/app/Models/DanhGia.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DanhGia extends Model
+{
+    protected $table = 'danh_gia';
+
+    protected $fillable = [
+        'nguoi_dung_id',
+        'phim_id',
+        'so_sao',
+        'noi_dung',
+    ];
+
+    public function nguoiDung()
+    {
+        return $this->belongsTo(NguoiDung::class, 'nguoi_dung_id');
+    }
+
+    public function phim()
+    {
+        return $this->belongsTo(Phim::class, 'phim_id');
+    }
+}
